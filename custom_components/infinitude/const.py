eb505636fabfe83f8b8e1a70362f43773df04d0b
MizterB <5458030+MizterB@users.noreply.github.com>
2024-01-31 14:52:38 -0500

    HA Presets

--- custom_components/infinitude/const.py
+++ custom_components/infinitude/const.py
@@ -1,3 +1,16 @@
 """Constants for the Infinitude integration."""
 
+from enum import Enum
+
 DOMAIN = "infinitude"
+
+
+class Preset(Enum):
+    """Climate presets supported by integration."""
+
+    SCHEDULE = "schedule"
+    HOME = "home"
+    AWAY = "away"
+    SLEEP = "sleep"
+    WAKE = "wake"
+    MANUAL = "override"
