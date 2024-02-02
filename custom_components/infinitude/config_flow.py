21113a761025106a413ce7f09ea1ffe3cbe7d84b
MizterB <5458030+MizterB@users.noreply.github.com>
2024-02-01 20:24:20 -0500

    Enable config flow

--- custom_components/infinitude/config_flow.py
+++ custom_components/infinitude/config_flow.py
@@ -4,83 +4,46 @@ from __future__ import annotations
 import logging
 from typing import Any
 
-from aiohttp import ClientConnectionError
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.const import CONF_HOST, CONF_PORT
+from homeassistant.const import CONF_HOST, CONF_PORT, CONF_SSL
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import DOMAIN
+from .infinitude.api import Infinitude
 
 _LOGGER = logging.getLogger(__name__)
 
 CONNECTION_SCHEMA = vol.Schema(
     {
         vol.Required(CONF_HOST): str,
-        vol.Required(CONF_PORT): int,
+        vol.Required(CONF_PORT, default=3000): int,
+        vol.Required(CONF_SSL, default=False): bool,
     }
 )
 
 
-class PlaceholderHub:
-    """Placeholder class to make tests pass.
-
-    TODO Remove this placeholder class and replace with things from your PyPI package.
-    """
-
-    def __init__(self, host: str) -> None:
-        """Initialize."""
-        self.host = host
-
-    async def authenticate(self, username: str, password: str) -> bool:
-        """Test if we can authenticate with the host."""
-        return True
-
-
-async def verify_connection(hass, host, port):
-    """Verify that Infinitude is available by requesting config data."""
-    session = async_get_clientsession(hass)
-    url = f"http://{CONF_HOST}:{CONF_PORT}/api/config"
-
-    try:
-        async with session.get(url) as resp:
-            if resp.status != 200:
-                raise CannotConnect
-    except ClientConnectionError:
-        raise CannotConnect
-
-    return resp
-
-
-async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
+async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> None:
     """Validate the user input allows us to connect.
 
     Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
     """
-    # TODO validate the data can be used to set up a connection.
-
-    # If your PyPI package is not built with async, pass your methods
-    # to the executor:
-    # await hass.async_add_executor_job(
-    #     your_validate_func, data[CONF_USERNAME], data[CONF_PASSWORD]
-    # )
-
-    hub = PlaceholderHub(data[CONF_HOST])
-
-    if not await hub.authenticate(data[CONF_USERNAME], data[CONF_PASSWORD]):
-        raise InvalidAuth
-
-    # If you cannot connect:
-    # throw CannotConnect
-    # If the authentication is wrong:
-    # InvalidAuth
-
-    # Return info that you want to store in the config entry.
-    return {"title": "Name of the device"}
+    session = async_get_clientsession(hass)
+    infinitude = Infinitude(
+        host=data[CONF_HOST],
+        port=data[CONF_PORT],
+        ssl=data[CONF_SSL],
+        session=session,
+    )
+    try:
+        await infinitude.connect()
+    except ConnectionError as connection_error:
+        _LOGGER.error("Error connecting to Infinitude: %s", connection_error)
+        raise CannotConnect from connection_error
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -94,18 +57,21 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle the initial step."""
         errors: dict[str, str] = {}
         if user_input is not None:
+            unique_id = f"{user_input[CONF_HOST]}:{user_input[CONF_PORT]}"
+            await self.async_set_unique_id(unique_id)
+            self._abort_if_unique_id_configured()
             try:
-                info = await validate_input(self.hass, user_input)
+                await validate_input(self.hass, user_input)
             except CannotConnect:
                 errors["base"] = "cannot_connect"
             except Exception:  # pylint: disable=broad-except
                 _LOGGER.exception("Unexpected exception")
                 errors["base"] = "unknown"
             else:
-                return self.async_create_entry(title=info["title"], data=user_input)
+                return self.async_create_entry(title=unique_id, data=user_input)
 
         return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            step_id="user", data_schema=CONNECTION_SCHEMA, errors=errors
         )
 
 
